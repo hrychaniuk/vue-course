5c5e825efd73a4476c672da972d59b280be74812
dimoto <grychanyukdo@gmail.com>
2020-01-11 16:14:08 +0200

    Add /dist

---
+++ dist/js/chunk-2d0d0b4d.00fa274f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d0b4d"],{"68b7":function(e,n,t){"use strict";t.r(n);var l=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",[t("h1",[e._v("BLog")]),t("ul",[t("li",[e._v("1")]),t("li",[e._v("2")])])])}],u={},r=u,i=t("2877"),s=Object(i["a"])(r,l,c,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0d0b4d.00fa274f.js.map
